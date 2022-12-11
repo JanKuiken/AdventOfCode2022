4a2c01461b95b55e641e5fb7c5f12aa842721bb8
Jan Kuiken <jan.kuiken@quicknet.nl>
2022-12-11 19:42:48 +0100

    dag 11, werkt nog niet

---
+++ day_11/step_1.py
@@ -0,0 +1,133 @@
+
+# there are 8 monkeys in the input file, we're not going to parse the file, we
+# copy/pste in in here and convert it to code...
+
+from dataclasses import dataclass
+from typing import Callable
+
+@dataclass
+class Monkey:
+    items: list
+    operation: Callable
+    test: Callable
+    true_monkey: int 
+    false_monkey: int
+    inspections: int
+
+monkeys = []
+
+monkeys.append(Monkey(
+    items        = [85, 79, 63, 72],
+    operation    = lambda old: old * 17,
+    test         = lambda x: x % 2 == 0,
+    true_monkey  = 2,
+    false_monkey = 6,
+    inspections  = 0,
+))
+
+monkeys.append(Monkey(
+    items        = [53, 94, 65, 81, 93, 73, 57, 92],
+    operation    = lambda old: old * old,
+    test         = lambda x: x % 7 == 0,
+    true_monkey  = 0,
+    false_monkey = 2,
+    inspections  = 0,
+))
+
+monkeys.append(Monkey(
+    items        = [62, 63],
+    operation    = lambda old: old + 7,
+    test         = lambda x: x % 13 == 0,
+    true_monkey  = 7,
+    false_monkey = 6,
+    inspections  = 0,
+))
+
+monkeys.append(Monkey(
+    items        = [57, 92, 56],
+    operation    = lambda old: old + 4,
+    test         = lambda x: x % 5 == 0,
+    true_monkey  = 4,
+    false_monkey = 5,
+    inspections  = 0,
+))
+
+monkeys.append(Monkey(
+    items        = [67],
+    operation    = lambda old: old + 5,
+    test         = lambda x: x % 3 == 0,
+    true_monkey  = 1,
+    false_monkey = 5,
+    inspections  = 0,
+))
+
+monkeys.append(Monkey(
+    items        = [85, 56, 66, 72, 57, 99],
+    operation    = lambda old: old + 6,
+    test         = lambda x: x % 19 == 0,
+    true_monkey  = 1,
+    false_monkey = 0,
+    inspections  = 0,
+))
+
+monkeys.append(Monkey(
+    items        = [86, 65, 98, 97, 69],
+    operation    = lambda old: old * 13,
+    test         = lambda x: x % 11 == 0,
+    true_monkey  = 3,
+    false_monkey = 7,
+    inspections  = 0,
+))
+
+monkeys.append(Monkey(
+    items        = [87, 68, 92, 66, 91, 50, 68],
+    operation    = lambda old: old + 2,
+    test         = lambda x: x % 17 == 0,
+    true_monkey  = 4,
+    false_monkey = 3,
+    inspections  = 0,
+))
+
+# oke, let's hope i typed all this correctly...
+
+# let's go
+for r in range(20):
+    for m, monkey in enumerate(monkeys):
+        while len(monkey.items):
+        
+            worry_level_item = monkey.items.pop()
+            worry_level = monkey.operation(worry_level_item)
+            worry_level = round(worry_level / 3)  # relax it a bit
+            test        = monkey.test(worry_level)
+            # throw it:
+            monkeys[monkey.true_monkey if test else monkey.false_monkey].items.append(worry_level)
+            # some admin
+            monkey.inspections += 1
+            # some debug console output
+            print( str(r), str(m), str(worry_level_item), str(worry_level),
+                   str(test), str(monkey.true_monkey if test else monkey.false_monkey),
+                   str(monkey.inspections))   
+
+# That caused, some trouble, i started out with a namedtuple, but had to change it
+# to a dataclass because a tuple cannot be changed (oh oh oh oh....)
+# Than i had to google how to add a lambda to a data class
+
+# any find the AOC answer and submit it...
+n_of_inspections = []
+for monkey in monkeys:
+    n_of_inspections.append(monkey.inspections)
+n_of_inspections.sort()
+print(n_of_inspections)
+monkey_business = n_of_inspections[-1] * n_of_inspections[-2]
+print(monkey_business)
+
+# i submitted 124605, it was wrong, too high, hmm start debugging....
+# (start with test data? check definitions?....)
+
+
+
+
+
+
+
+
